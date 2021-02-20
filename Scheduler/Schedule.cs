529be52749a0020de3c46865c527050083daf0f9
kgazha <kgazha@gmail.com>
2021-02-20 22:41:26 +0500

    Fix nearest date search method Add new tests.

--- Scheduler/Schedule.cs
+++ Scheduler/Schedule.cs
@@ -8,6 +8,10 @@ namespace Scheduler
 {
 	public class Schedule
 	{
+		const int MinYear = 2000;
+		const int MaxYear = 2100;
+		const int FirstMonth = 1;
+		const int LastMonth = 12;
 		public IDateTimeParser parser = new DateTimeParser(new StringParser());
 		public ISequence sequence = new SequenceCreator();
 		string defaultSchedule = "*.*.* * *:*:*.*";
@@ -22,31 +26,52 @@ namespace Scheduler
 			parser.Parse(scheduleString);
 		}
 
+		/// <summary>
+		/// Возвращает следующий ближайший к заданному времени момент в расписании или
+		/// само заданное время, если оно есть в расписании.
+		/// </summary>
+		/// <param name="t1">Заданное время</param>
+		/// <returns>Ближайший момент времени в расписании</returns>
 		public DateTime NearestEvent(DateTime t1)
 		{
 			DateTime result = new DateTime();
-			var years = sequence.GenerateSequence(parser.Year, 2000, 2100);
+
+			result = GetNearestDate(t1);
+			return result;
+		}
+
+		private DateTime GetNearestDate(DateTime t1)
+		{
+			DateTime result = new DateTime();
+			var years = sequence.GenerateSequence(parser.Year, MinYear, MaxYear);
 			int year = years.Aggregate((x, y) => Math.Abs(x - t1.Year) <= Math.Abs(y - t1.Year) ? x : y);
-			var months = sequence.GenerateSequence(parser.Month, 1, 12);
+			var months = sequence.GenerateSequence(parser.Month, FirstMonth, LastMonth);
 			int month = 0;
-            if (year < t1.Year)
-            {
+			if (year < t1.Year)
+			{
 				month = months.Aggregate((x, y) => Math.Abs(x - t1.Month) >= Math.Abs(y - t1.Month) ? x : y);
 			}
 			else
-            {
+			{
 				month = months.Aggregate((x, y) => Math.Abs(x - t1.Month) <= Math.Abs(y - t1.Month) ? x : y);
 			}
 			int day = 0;
-            if (parser.Day == "32")
-            {
+			if (parser.Day == "32")
+			{
 				day = DateTime.DaysInMonth(year, month);
 			}
 			else
-            {
+			{
 				var days = sequence.GenerateSequence(parser.Day, 1, DateTime.DaysInMonth(year, month));
-				day = days.Aggregate((x, y) => Math.Abs(x - t1.Day) <= Math.Abs(y - t1.Day) ? x : y);
-            }
+                if (month < t1.Month || year < t1.Year)
+                {
+					day = days.Aggregate((x, y) => Math.Abs(x - t1.Day) >= Math.Abs(y - t1.Day) ? x : y);
+                }
+                else
+                {
+					day = days.Aggregate((x, y) => Math.Abs(x - t1.Day) <= Math.Abs(y - t1.Day) ? x : y);
+				}
+			}
 			result = new DateTime(year: year, month: month, day: day);
 			return result;
 		}
--- TestScheduler/ScheduleTest.cs
+++ TestScheduler/ScheduleTest.cs
@@ -12,13 +12,31 @@ namespace TestScheduler
         public void NearestEventTest()
         {
             Schedule schedule = new Schedule();
-            var result = schedule.NearestEvent(DateTime.Now);
+            var result = schedule.NearestEvent(new DateTime(year: 2021, month: 2, day: 20));
             var expected = new DateTime(year: 2021, month: 2, day: 20);
             Assert.Equal(expected, result);
 
             schedule = new Schedule("2019-2020.10-12.20");
-            result = schedule.NearestEvent(DateTime.Now);
+            result = schedule.NearestEvent(new DateTime(year: 2021, month: 2, day: 20));
             expected = new DateTime(year: 2020, month: 12, day: 20);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ClosestNextDateTest()
+        {
+            var schedule = new Schedule("2019-2021.10-12.20,24-27");
+            var result = schedule.NearestEvent(new DateTime(year: 2021, month: 4, day: 20));
+            var expected = new DateTime(year: 2021, month: 10, day: 20);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ClosestPreviousDateTest()
+        {
+            var schedule = new Schedule("2019-2020.1,2,09-12.20,24-27");
+            var result = schedule.NearestEvent(new DateTime(year: 2021, month: 4, day: 20));
+            var expected = new DateTime(year: 2020, month: 12, day: 27);
             Assert.Equal(expected, result);
         }
     }
